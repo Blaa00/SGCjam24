356acf5b8e3cc47559ecc3d5520271c8f5b0813a
Eken2008 <oskar.eklov@gmail.com>
2024-04-05 21:43:22 +0200

    added tile placing

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import copy
+from enum import Enum, auto
 import pygame
 
 pygame.display.init()
 window=pygame.display.set_mode((16*70,9*70))
 
+class EDGES(Enum):
+    grass=auto()
+
 class Tile:
-    def __init__(self,imgpath) -> None:
-        self.img=pygame.image.load("assets/"+imgpath).convert_alpha()
+    def __init__(self,imgpath,right=EDGES.grass,left=EDGES.grass,top=EDGES.grass,bottom=EDGES.grass) -> None:
+        self.img=pygame.transform.scale(pygame.image.load("assets/"+imgpath).convert_alpha(),(64,64))
         self.x=0
         self.y=0
 
-    def render(self,holding=False):
+    def render(self,x,y,holding=False):
         if holding:
             img=pygame.transform.scale(self.img,(96,96))
             img.set_alpha(180)
-            window.blit(img,((self.x*64)+((window.get_width()-96)/2),((self.y*64)+((window.get_height()-96)/2))))
+            window.blit(img,((x*64)+((window.get_width()-96)/2),((y*64)+((window.get_height()-96)/2))))
             return
-        window.blit(self.img,((self.x*64)+((window.get_width()-64)/2),((self.y*64)+((window.get_height()-64)/2))))
+        window.blit(self.img,((x*64)+((window.get_width()-64)/2),((y*64)+((window.get_height()-64)/2))))
 
 try:
     grass = Tile("grass.png")
@@ -24,8 +28,10 @@ except FileNotFoundError:
     raise NotImplementedError("This error message will be better.")
 
 
-cursor=copy.copy(grass)
+cursor=grass
 
+
+world = {"0,0":grass}
 
 while True:
     EVENTS = pygame.event.get()
@@ -36,8 +42,25 @@ while True:
 
     window.fill("#000000")
 
-    grass.render()
-    cursor.x=1
-    cursor.render(True)
+    
+    for pos, tile in world.items():
+        tile.render(int(pos.split(",")[0]),int(pos.split(",")[1]))
+
+    cx=int((pygame.mouse.get_pos()[0]+32-window.get_width()/2)//64)
+    cy=int((pygame.mouse.get_pos()[1]+32-window.get_height()/2)//64)
+    if pygame.mouse.get_pressed()[0]:
+        if not f"{cx},{cy}" in world:
+            blockLeft=world.get(f"{cx-1},{cy}")
+            blockRight=world.get(f"{cx+1},{cy}")
+            blockTop=world.get(f"{cx},{cy-1}")
+            blockBottom=world.get(f"{cx},{cy+1}")
+
+            allow=True
+            if blockLeft==None and blockRight==None and blockTop==None and blockBottom==None:allow=False
+
+            if allow:
+                world[f"{cx},{cy}"]=cursor
+
+    cursor.render(cx,cy,True)
 
     pygame.display.flip()
